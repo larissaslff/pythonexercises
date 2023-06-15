1f48a4c3ebbfcaacc42357d12273b5e642b54506
larissaslff <110000521+larissaslff@users.noreply.github.com>
2023-06-15 20:10:02 -0300

    add python exercises

---
+++ Exercicio08.py
@@ -0,0 +1,4 @@
+n = float(input('Diga o valor: '))
+centimetros = n*100
+milimetros = n*1000
+print('O valor {} equivale a {} centimetros e {} milimetros' .format(n, centimetros, milimetros))
---
+++ Exercicio09.py
@@ -0,0 +1,8 @@
+n = int(input('Digite o numero: '))
+print('{} x 0 = {}' .format(n, n*0))
+print('{} x 1 = {}' .format(n, n*1))
+print('{} x 2 = {}' .format(n, n*2))
+print('{} x 3 = {}' .format(n, n*3))
+print('{} x 4 = {}' .format(n, n*4))
+print('{} x 5 = {}' .format(n, n*5))
+print('{} x 6 = {}' .format(n, n*6))
